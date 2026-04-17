4b53b7657364e1a7ee363ffcd44de84751797c80
Krinkovic <186858802+Krinkovic@users.noreply.github.com>
2026-04-17 20:34:14 +0200

    setup skeleton for the input generation

---
+++ src/input_gen.c
@@ -0,0 +1,31 @@
+// Kristoffer & Abdulsalam
+
+#include <stdlib.h>
+
+unsigned int* ordered(int n)
+{
+    unsigned int * arr = malloc(sizeof(unsigned int) * n);
+    //TODO
+    return arr;
+}
+
+unsigned int* reverse(int n)
+{
+    unsigned int * arr = malloc(sizeof(unsigned int) * n);
+    //TODO
+    return arr;
+}
+
+unsigned int* randomized(int n)
+{
+    unsigned int * arr = malloc(sizeof(unsigned int) * n);
+    //TODO
+    return arr;
+}
+
+unsigned int* almost_ordered(int n)
+{
+    unsigned int * arr = malloc(sizeof(unsigned int) * n);
+    //TODO
+    return arr;
+}
