57399f5940891ee1b365f81600cf8d9ceba815e6
Krinkovic <186858802+Krinkovic@users.noreply.github.com>
2026-04-17 20:50:39 +0200

    Updated error. Added test to see everything works

--- src/input_gen.c
+++ src/input_gen.c
@@ -1,18 +1,25 @@
 // Kristoffer & Abdulsalam
 
 #include <stdlib.h>
+#include <stdio.h>
 
 unsigned int* ordered(int n)
 {
+    //TODO: make it more interesting, not just a linear list of nums
     unsigned int * arr = malloc(sizeof(unsigned int) * n);
-    //TODO
+    for (int i = 0; i < n; i++) {
+        arr[i] = i + 1; // As basic as can be, output will be [1,2,3,...,n]
+    }
     return arr;
 }
 
 unsigned int* reverse(int n)
 {
+    //TODO: make it more interesting, not just a linear list of nums
     unsigned int * arr = malloc(sizeof(unsigned int) * n);
-    //TODO
+    for (int i = 0; i < n; i++) {
+        arr[i] = n - i; // As basic as can be, output will be [n,n-1,n-2,...,3,2,1]
+    }
     return arr;
 }
 
@@ -28,4 +35,22 @@ unsigned int* almost_ordered(int n)
     unsigned int * arr = malloc(sizeof(unsigned int) * n);
     //TODO
     return arr;
+}
+
+int main(void) // For testing
+{
+    int n = 5;
+    unsigned int* arr1 = ordered(n);
+    unsigned int* arr2 = reverse(n);
+
+    puts("Ordered:");
+    for (int i = 0; i < n; i++) {
+        printf("%d\n", arr1[i]);
+    }
+    puts("Reverse:");
+    for (int i = 0; i < n; i++) {
+        printf("%d\n", arr2[i]);
+    }
+    free(arr1);
+    free(arr2);
 }
