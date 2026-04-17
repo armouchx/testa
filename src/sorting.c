ac9e20f75a550189dd9084dc6fd252784ce3fd5c
Krinkovic <186858802+Krinkovic@users.noreply.github.com>
2026-04-17 20:07:24 +0200

    Added bubble sort to sorting.c, modified to count basic operations. Corrected wrong location of gitignore and updated it.

---
+++ src/sorting.c
@@ -0,0 +1,53 @@
+// Kristoffer & Abdulsalam
+
+#include <stdio.h>
+
+void bubbleSort(int arr[], int n, int *op);
+
+int main(void) // To test that it works
+{
+    int arr1[] = {6,3,9,10,1,3};
+    int arr2[] = {1,3,9,10,11,13};
+    int arr3[] = {13,11,10,9,3,1};
+    int n = 6;
+    int op = 0;
+
+    bubbleSort(arr1, n, &op);
+    printf("number of operations: %d\n", op);
+    printf("sorted numbers:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d\n", arr1[i]);
+    }
+    puts("");
+
+    op = 0;
+    bubbleSort(arr2, n, &op);
+    printf("number of operations: %d\n", op);
+    printf("sorted numbers:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d\n", arr2[i]);
+    }
+    puts("");
+
+    op = 0;
+    bubbleSort(arr3, n, &op);
+    printf("number of operations: %d\n", op);
+    printf("sorted numbers:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d\n", arr3[i]);
+    }
+}
+
+void bubbleSort(int arr[], int n, int *op)
+{
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - 1; j++) {
+            *op += 1;
+            if (arr[j + 1] < arr[j]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
