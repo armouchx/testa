c44006f0bffa4c70f5503e518ea2c1e3f8f50d40
Krinkovic <186858802+Krinkovic@users.noreply.github.com>
2026-04-17 19:23:08 +0200

    Finished bubble sort, and added a .gitignore file. Allows us to tell git what files we dont want to upload. For example here I added "bin/" folder. I make all my compiled binary files go to that folder, so now they dont get uploaded. Just a waste of space to upload compiled binaries

---
+++ task_1/bubble_sort.c
@@ -0,0 +1,40 @@
+// Kristoffer & Abdulsalam
+
+#include <stdio.h>
+
+void bubbleSort(int arr[], int n);
+
+int main(void) // To test that it works
+{
+    int arr[] = {6,3,9,10,1,3};
+    int n = 6;
+
+    bubbleSort(arr, n);
+    printf("sorted numbers:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d\n", arr[i]);
+    }
+}
+
+// Bubble sort
+// Sorts a given array by bubble sort
+// Input: An array A[0..n − 1] of orderable elements
+// Output: Array A[0..n − 1] sorted in nondecreasing order
+//
+// BubbleSort(A[0..n − 1])
+//     for i ← 0 to n − 2 do
+//         for j ← 0 to n − 2 − i do
+//             if A[j + 1] < A[j] swap A[j] and A[j + 1]
+
+void bubbleSort(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - 1; j++) {
+            if (arr[j + 1] < arr[j]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
