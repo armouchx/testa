a89dfe53edd80115b9e8a6bfe4f0b87240a86a48
Krinkovic <186858802+Krinkovic@users.noreply.github.com>
2026-04-19 15:28:27 +0200

    finished remaining generation functions and made the existing ones use random values as well. Can adjust variability with variable

--- src/input_gen.c
+++ src/input_gen.c
@@ -2,46 +2,88 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
+
 
 unsigned int* ordered(int n)
 {
-    //TODO: make it more interesting, not just a linear list of nums
+    srand(time(NULL));
     unsigned int * arr = malloc(sizeof(unsigned int) * n);
+
+    int variance = 11;
+    int prev = 0;
+
     for (int i = 0; i < n; i++) {
-        arr[i] = i + 1; // As basic as can be, output will be [1,2,3,...,n]
+        arr[i] = prev + rand() % variance; // Randomized so each number will be a random number bigger than the previous, based on variance
+        prev = arr[i];
     }
     return arr;
 }
 
 unsigned int* reverse(int n)
 {
-    //TODO: make it more interesting, not just a linear list of nums
+    srand(time(NULL));
     unsigned int * arr = malloc(sizeof(unsigned int) * n);
-    for (int i = 0; i < n; i++) {
-        arr[i] = n - i; // As basic as can be, output will be [n,n-1,n-2,...,3,2,1]
+
+    int variance = 11;
+    int prev = 0;
+
+    for (int i = n; i >= 0; i--) {
+        arr[i] = prev + rand() % variance; // Same as above, but reverse
+        prev = arr[i];
     }
     return arr;
 }
 
 unsigned int* randomized(int n)
 {
+    srand(time(NULL));
     unsigned int * arr = malloc(sizeof(unsigned int) * n);
-    //TODO
+
+    int variance = 10;
+
+    for (int i = 0; i < n; i++) {
+        arr[i] = rand() % (n * variance); // Random numbers between 0 and n * variance
+    }
     return arr;
 }
 
 unsigned int* almost_ordered(int n)
 {
+    srand(time(NULL));
     unsigned int * arr = malloc(sizeof(unsigned int) * n);
+
+    int variance = 11;
+    int prev = 0;
+    int swaps = n / 100 * 4 / 2;
+    printf("swaps = %d\n", swaps);
+
+    for (int i = 0; i < n; i++) {
+        arr[i] = prev + rand() % variance; // Randomized so each number will be a random number bigger than the previous, based on variance
+        prev = arr[i];
+    }
+
+    for (int i = 0; i < swaps; i++) { // swap ≈4% of items
+        int i1 = rand() % n;
+        printf("i1 = %d, arr[i1] = %d\n", i1, arr[i1]);
+        int i2 = rand() % n;
+        printf("i2 = %d, arr[i2] = %d\n", i2, arr[i2]);
+        printf("swapping %d(%d) with %d(%d)\n", i1, arr[i1], i2, arr[i2]);
+        int temp = arr[i1];
+        arr[i1] = arr[i2];
+        arr[i2] = temp;
+    }
     //TODO
     return arr;
 }
 
 int main(void) // For testing
 {
-    int n = 5;
+    int n = 100;
     unsigned int* arr1 = ordered(n);
     unsigned int* arr2 = reverse(n);
+    unsigned int* arr3 = randomized(n);
+    unsigned int* arr4 = almost_ordered(n);
 
     puts("Ordered:");
     for (int i = 0; i < n; i++) {
@@ -51,6 +93,16 @@ int main(void) // For testing
     for (int i = 0; i < n; i++) {
         printf("%d\n", arr2[i]);
     }
+    puts("Randomized:");
+    for (int i = 0; i < n; i++) {
+        printf("%d\n", arr3[i]);
+    }
+    puts("Almost ordered:");
+    for (int i = 0; i < n; i++) {
+        printf("%d\n", arr4[i]);
+    }
     free(arr1);
     free(arr2);
+    free(arr3);
+    free(arr4);
 }
